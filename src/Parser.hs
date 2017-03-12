a417f930d8e05df4e37578b8d91cd5332b34983c
tyfkda <tyfkda@gmail.com>
2017-03-12 13:28:39 +0900

    Expand rotated and flipped shapes (not implemented yet)

--- src/Parser.hs
+++ src/Parser.hs
@@ -5,11 +5,14 @@ module Parser
     , parsePiece
     ) where
 
+import Prelude hiding (flip)
+import Data.List (nub)
 import Data.Text (split, pack, unpack)
 
 type Location = (Int, Int)  -- row, column
 type Color = Char  -- Piece color
-data Piece = Piece Color Bool Bool [Location]  -- Color, Rotation enable?, Flip enable?, Locations of block
+type Shape = [Location]  -- Locations of block
+data Piece = Piece Color [Shape]  -- Color, Locations of block
   deriving Show
 
 type Size = (Int, Int)  -- row, column
@@ -23,23 +26,39 @@ parse contents = (Board (rows, cols) startPoints, pieces)
         startPoints = map parseStartLocation $ words startPointsLine
         pieces = map parsePiece rs
 
-splitString sep str = map unpack $ split (== sep) $ pack str
-
 -- ex. "G:1,4" -> ('G', (1, 4))
 parseStartLocation :: String -> (Color, Location)
 parseStartLocation ss = (col, loc)
   where (col : ':' : rs) = ss
         loc = parseLocation rs
 
--- ex. "G . . 1,1, 1,2 1,3 2,2" -> Piece 'G' False False [(1,1),(1,2),(1,3),(2,2)]
+-- ex. "G . . 1,1, 1,2 1,3 2,2" -> Piece 'G' [[(1,1),(1,2),(1,3),(2,2)]]
 parsePiece :: String -> Piece
-parsePiece ss = Piece col rotate flip locs
+parsePiece ss = Piece col (expandShape canRotate canFlip shape)
   where (cs : rs : fs : ls) = words ss
         col = head cs
-        rotate = rs /= "."
-        flip = fs /= "."
-        locs = map parseLocation ls
+        canRotate = rs /= "."
+        canFlip = fs /= "."
+        shape = parseShape ls
+
+expandShape :: Bool -> Bool -> Shape -> [Shape]
+expandShape canRotate canFlip shape = nub flipped
+  where rotated
+          | canRotate  = rotate shape
+          | otherwise  = [shape]
+        flipped
+          | canFlip    = rotated ++ map flip rotated
+          | otherwise  = rotated
+
+rotate = undefined
+flip = undefined
+
+parseShape :: [String] -> Shape
+parseShape = map parseLocation
 
 -- ex. "1,4" -> (1, 4)
 parseLocation :: String -> Location
 parseLocation = (\(r : c : _) -> (read r, read c)) . splitString ','
+
+splitString :: Char -> String -> [String]
+splitString sep str = map unpack $ split (== sep) $ pack str
