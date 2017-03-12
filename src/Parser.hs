2c400cb55b2d07ed57ea5a2f2bdcef0707905e14
tyfkda <tyfkda@gmail.com>
2017-03-12 11:53:18 +0900

    Parse pieces

--- src/Parser.hs
+++ src/Parser.hs
@@ -2,6 +2,7 @@ module Parser
     ( parse
     -- For unit test
     , parseStartLocation
+    , parsePiece
     ) where
 
 import Data.Text (split, pack, unpack)
@@ -15,11 +16,12 @@ type Size = (Int, Int)  -- row, column
 data Board = Board Size [(Color, Location)]  -- Size, Start positions for each letters.
   deriving Show
 
-parse :: String -> Board
-parse contents = Board (rows, cols) startPoints
+parse :: String -> (Board, [Piece])
+parse contents = (Board (rows, cols) startPoints, pieces)
   where (boardLine : startPointsLine : rs) = lines contents
         [rows, cols] = map read $ words boardLine
         startPoints = map parseStartLocation $ words startPointsLine
+        pieces = map parsePiece rs
 
 splitString sep str = map unpack $ split (== sep) $ pack str
 
@@ -27,4 +29,17 @@ splitString sep str = map unpack $ split (== sep) $ pack str
 parseStartLocation :: String -> (Color, Location)
 parseStartLocation ss = (col, loc)
   where (col : ':' : rs) = ss
-        loc = (\(r : c : _) -> (read r, read c)) $ splitString ',' rs
+        loc = parseLocation rs
+
+-- ex. "G . . 1,1, 1,2 1,3 2,2" -> Piece 'G' False False [(1,1),(1,2),(1,3),(2,2)]
+parsePiece :: String -> Piece
+parsePiece ss = Piece col rotate flip locs
+  where (cs : rs : fs : ls) = words ss
+        col = head cs
+        rotate = rs /= "."
+        flip = fs /= "."
+        locs = map parseLocation ls
+
+-- ex. "1,4" -> (1, 4)
+parseLocation :: String -> Location
+parseLocation = (\(r : c : _) -> (read r, read c)) . splitString ','
