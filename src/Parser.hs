9299502f4d409e4df9789a6830e59607bbf50595
tyfkda <tyfkda@gmail.com>
2017-03-13 15:00:01 +0900

    Create a list of functions to modify a shape to rotate/flip

--- src/Parser.hs
+++ src/Parser.hs
@@ -5,7 +5,6 @@ module Parser
     , parsePiece
     ) where
 
-import Prelude hiding (flip)
 import Data.List (nub, sort)
 import Data.Text (split, pack, unpack)
 
@@ -26,7 +25,7 @@ parseStartPos ss = (col, pos)
 
 -- ex. "G . . 1,1, 1,2 1,3 2,2" -> Piece 'G' [[(0,0),(0,1),(0,2),(1,1)]]
 parsePiece :: String -> Piece
-parsePiece ss = Piece col (expandShape canRotate canFlip shape)
+parsePiece ss = Piece col $ expandShape canRotate canFlip shape
   where (cs : rs : fs : ls) = words ss
         col = head cs
         canRotate = rs /= "."
@@ -34,20 +33,21 @@ parsePiece ss = Piece col (expandShape canRotate canFlip shape)
         shape = parseShape ls
 
 expandShape :: Bool -> Bool -> Shape -> [Shape]
-expandShape canRotate canFlip shape = nub $ map sort flipped
-  where rotated
-          | canRotate  = take 4 $ iterate rotate90 shape
-          | otherwise  = [shape]
-        flipped
-          | canFlip    = rotated ++ map flip rotated
-          | otherwise  = rotated
+expandShape canRotate canFlip shape = nub $ [normalizeShape (f shape) | f <- modifiers]
+  where modifiers = [ff . fr | ff <- flipFuncs, fr <- rotFuncs]
+        rotFuncs
+          | canRotate  = take 4 $ iterate (rotate90 .) id
+          | otherwise  = [id]
+        flipFuncs
+          | canFlip    = [id, flipHorz]
+          | otherwise  = [id]
 
 rotate90 :: Shape -> Shape
-rotate90 shape = normalizeShape $ map rot90 shape
+rotate90 shape = map rot90 shape
   where rot90 (r, c) = (-c, r)
 
-flip :: Shape -> Shape
-flip shape = normalizeShape $ map f shape
+flipHorz :: Shape -> Shape
+flipHorz shape = map f shape
   where f (r, c) = (-r, c)
 
 -- Move a shape to fit the origin
